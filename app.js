8f92ba3723e6eaa8a192db310302b90358ab96d4
Jaspreet Kaur <jaspreetr86@gmail.com>
2018-10-06 23:00:19 -0400

    update link for client/build

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.use(bodyParser.json());
 
 if(process.env.NODE_ENV === "production")
 {
-    app.use(express.static("client/public"));
+    app.use(express.static("client/build"));
 }
 
 app.use(routes);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/reactnyt";
